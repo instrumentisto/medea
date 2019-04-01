db419cd556af9eab6f6ce6e11442f9f19b7a084f
alexlapa <lapa.alex@ex.ua>
2019-04-01 22:07:51 +0300

    remove cfg_if

--- medea-client/src/lib.rs
+++ medea-client/src/lib.rs
@@ -1,16 +1,19 @@
-mod utils;
-
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-cfg_if::cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
+// When the `console_error_panic_hook` feature is enabled, we can call the
+// `set_panic_hook` function at least once during initialization, and then
+// we will get better error messages if our code ever panics.
+//
+// For more details see
+// https://github.com/rustwasm/console_error_panic_hook#readme
+#[cfg(feature = "console_error_panic_hook")]
+pub use console_error_panic_hook::set_once as set_panic_hook;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct Medea {
@@ -21,6 +24,7 @@ pub struct Medea {
 impl Medea {
     #[wasm_bindgen(constructor)]
     pub fn new(token: String) -> Self {
+        set_panic_hook();
         Self { token }
     }
 
