0d26545034f955275629f74e6ba143eb43a27c12
alexlapa <lapa.alex@ex.ua>
2019-04-08 20:33:35 +0300

    copy protocol messages

--- jason/e2e/js/index.js
+++ jason/e2e/js/index.js
@@ -1,11 +1,14 @@
 async function f() {
     const rust = await import("../../pkg");
 
-    let jason = new rust.Jason();
+    let caller = new rust.Jason();
 
-    let session = jason.init_session("ws://localhost:8080/ws/1/1/caller_credentials");
+    let caller_session = caller.init_session("ws://localhost:8080/ws/1/1/caller_credentials");
 
-    alert("hi");
+    let responder = new rust.Jason();
+
+    let responder_session = responder.init_session("ws://localhost:8080/ws/1/2/responder_credentials");
+
 }
 
 f();
--- jason/src/transport/protocol.rs
+++ jason/src/transport/protocol.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-pub struct Command {}
-
 #[derive(Deserialize, Serialize)]
 pub enum Heartbeat {
     /// `ping` message that WebSocket client is expected to send to the server
@@ -13,3 +11,71 @@ pub enum Heartbeat {
     #[serde(rename = "pong")]
     Pong(usize),
 }
+
+/// WebSocket message from Web Client to Media Server.
+#[derive(Debug, Serialize)]
+pub enum Command {
+    /// Web Client sends SDP Offer.
+    MakeSdpOffer { peer_id: u64, sdp_offer: String },
+    /// Web Client sends SDP Answer.
+    MakeSdpAnswer { peer_id: u64, sdp_answer: String },
+    /// Web Client sends Ice Candidate.
+    SetIceCandidate { peer_id: u64, candidate: String },
+}
+
+/// WebSocket message from Media Server to Web Client.
+#[derive(Debug, Deserialize)]
+pub enum Event {
+    /// Media Server notifies Web Client about necessity of RTCPeerConnection
+    /// creation.
+    PeerCreated {
+        peer_id: u64,
+        sdp_offer: Option<String>,
+        tracks: Vec<DirectionalTrack>,
+    },
+    /// Media Server notifies Web Client about necessity to apply specified SDP
+    /// Answer to Web Client's RTCPeerConnection.
+    SdpAnswerMade {
+        peer_id: u64,
+        sdp_answer: String,
+    },
+
+    IceCandidateDiscovered {
+        peer_id: u64,
+        candidate: String,
+    },
+
+    /// Media Server notifies Web Client about necessity of RTCPeerConnection
+    /// close.
+    PeersRemoved {
+        peer_ids: Vec<u64>,
+    },
+}
+
+/// [`Track] with specified direction.
+#[derive(Debug, Deserialize)]
+pub struct DirectionalTrack {
+    pub id: u64,
+    pub direction: TrackDirection,
+    pub media_type: TrackMediaType,
+}
+
+/// Direction of [`Track`].
+#[derive(Debug, Deserialize)]
+pub enum TrackDirection {
+    Send { receivers: Vec<u64> },
+    Recv { sender: u64 },
+}
+
+/// Type of [`Track`].
+#[derive(Debug, Deserialize)]
+pub enum TrackMediaType {
+    Audio(AudioSettings),
+    Video(VideoSettings),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AudioSettings {}
+
+#[derive(Debug, Deserialize)]
+pub struct VideoSettings {}
