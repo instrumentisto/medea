bd5fdaa9c025a27594738c54c0b4ed3b9c3c85ab
Kirguir <a.shutiy@gmail.com>
2019-04-17 10:50:01 +0300

    Fix docs

--- src/conf/coturn.rs
+++ src/conf/coturn.rs
@@ -8,12 +8,12 @@ use smart_default::*;
 /// COTURN server settings.
 #[derive(Clone, Debug, Deserialize, Serialize, SmartDefault)]
 pub struct Coturn {
-    /// IP address to bind COTURN server to. Defaults to `0.0.0.0`.
+    /// IP address COTURN server. Defaults to `0.0.0.0`.
     #[default(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)))]
-    pub bind_ip: IpAddr,
-    /// Port to bind COTURN server to. Defaults to `3478`.
+    pub ip: IpAddr,
+    /// Port to connect COTURN server. Defaults to `3478`.
     #[default(3478)]
-    pub bind_port: u16,
+    pub port: u16,
     /// Username for authorize on COTURN server.
     pub user: String,
     /// Password for authorize on COTURN server.
@@ -21,10 +21,10 @@ pub struct Coturn {
 }
 
 impl Coturn {
-    /// Builds [`SocketAddr`] from `bind_ip` and `bind_port`.
+    /// Builds [`SocketAddr`] from `ip` and `port`.
     #[inline]
-    pub fn get_bind_addr(&self) -> SocketAddr {
-        (self.bind_ip, self.bind_port)
+    pub fn get_addr(&self) -> SocketAddr {
+        (self.ip, self.port)
             .to_socket_addrs()
             .unwrap()
             .next()
@@ -44,21 +44,21 @@ mod coturn_spec {
 
     #[test]
     #[serial]
-    fn overrides_defaults_and_gets_bind_addr() {
+    fn overrides_defaults_and_gets_addr() {
         let default_conf = Conf::default();
 
-        env::set_var("MEDEA_COTURN.BIND_IP", "5.5.5.5");
-        env::set_var("MEDEA_COTURN.BIND_PORT", "1234");
+        env::set_var("MEDEA_COTURN.IP", "5.5.5.5");
+        env::set_var("MEDEA_COTURN.PORT", "1234");
 
         let env_conf = Conf::parse().unwrap();
 
-        assert_ne!(default_conf.coturn.bind_ip, env_conf.coturn.bind_ip);
-        assert_ne!(default_conf.coturn.bind_port, env_conf.coturn.bind_port);
+        assert_ne!(default_conf.coturn.ip, env_conf.coturn.ip);
+        assert_ne!(default_conf.coturn.port, env_conf.coturn.port);
 
-        assert_eq!(env_conf.coturn.bind_ip, Ipv4Addr::new(5, 5, 5, 5));
-        assert_eq!(env_conf.coturn.bind_port, 1234);
+        assert_eq!(env_conf.coturn.ip, Ipv4Addr::new(5, 5, 5, 5));
+        assert_eq!(env_conf.coturn.port, 1234);
         assert_eq!(
-            env_conf.coturn.get_bind_addr(),
+            env_conf.coturn.get_addr(),
             "5.5.5.5:1234".parse().unwrap(),
         );
     }
