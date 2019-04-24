db792d8f7d1ee825ed99fd1ab466077d33e21d62
alexlapa <lapa.alex@ex.ua>
2019-04-24 15:58:12 +0300

    refactor RpcClient.init

--- jason/src/rpc/mod.rs
+++ jason/src/rpc/mod.rs
@@ -52,33 +52,32 @@ impl RPCClient {
     }
 
     pub fn init(&mut self) -> Result<(), WasmErr> {
-        let socket = WebSocket::new(&self.token)?;
-        let socket = Rc::new(RefCell::new(Some(socket)));
-        let socket_borrow = socket.borrow();
+        self.sock = Rc::new(RefCell::new(Some(WebSocket::new(&self.token)?)));
+
+        let socket_borrow = self.sock.borrow();
         let socket_ref = socket_borrow
             .as_ref()
             .ok_or_else(|| WasmErr::from_str("socket is None"))?;
 
-        let socket_rc = Rc::clone(&socket);
-        let pinger_rc: Rc<Pinger> = Rc::clone(&self.pinger);
-
+        let socket = Rc::clone(&self.sock);
+        let pinger = Rc::clone(&self.pinger);
         socket_ref.on_open(move || {
-            if let Err(err) = pinger_rc.start(socket_rc) {
+            if let Err(err) = pinger.start(socket) {
                 err.log_err();
             };
         })?;
 
-        let pinger_rc = Rc::clone(&self.pinger);
-        let subs_rc = Rc::clone(&self.subs);
+        let pinger = Rc::clone(&self.pinger);
+        let subs = Rc::clone(&self.subs);
         socket_ref.on_message(move |msg: Result<InMsg, WasmErr>| {
             match msg {
                 Ok(InMsg::Pong(_num)) => {
                     // TODO: detect no pings
-                    pinger_rc.set_pong_at(Date::now());
+                    pinger.set_pong_at(Date::now());
                 }
                 Ok(InMsg::Event(event)) => {
                     // TODO: many subs, filter messages by session
-                    let subs_borrow = subs_rc.borrow();
+                    let subs_borrow = subs.borrow();
 
                     if let Some(sub) = subs_borrow.iter().next() {
                         if let Err(err) = sub.unbounded_send(event) {
@@ -96,11 +95,11 @@ impl RPCClient {
             }
         })?;
 
-        let socket_rc = Rc::clone(&socket);
-        let pinger_rc: Rc<Pinger> = Rc::clone(&self.pinger);
+        let socket = Rc::clone(&self.sock);
+        let pinger = Rc::clone(&self.pinger);
         socket_ref.on_close(move |msg: CloseMsg| {
-            socket_rc.borrow_mut().take();
-            pinger_rc.stop();
+            socket.borrow_mut().take();
+            pinger.stop();
 
             // TODO: reconnect on disconnect, propagate error if unable
             // to reconnect
@@ -108,9 +107,6 @@ impl RPCClient {
                 CloseMsg::Normal(_msg) | CloseMsg::Disconnect(_msg) => {}
             }
         })?;
-
-        drop(socket_borrow);
-        self.sock = socket;
 
         Ok(())
     }
