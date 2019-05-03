7cd3ff1657d61fb8b87b2931c279fe5ccc37728e
Kirguir <a.shutiy@gmail.com>
2019-05-03 15:53:41 +0300

    Impl on_message handler for RpcClient
    - add docs

--- jason/src/rpc/mod.rs
+++ jason/src/rpc/mod.rs
@@ -39,9 +39,15 @@ pub enum CloseMsg {
 // 4. Buffering if no socket?
 pub struct RPCClient(Rc<RefCell<Inner>>);
 
+/// Inner state of [`RPCClient`].
 struct Inner {
+    /// WebSocket connection to remote media server.
     sock: Option<Rc<WebSocket>>,
+
+    /// Credentials for authorize on remote media server.
     token: String,
+
+    /// Sends ping message to remote media server with specified interval.
     pinger: Pinger,
     subs: Vec<UnboundedSender<Event>>,
 }
@@ -57,6 +63,7 @@ impl Inner {
     }
 }
 
+/// Returns handler for bind on close WebSocket connection.
 fn on_close(inner_rc: Rc<RefCell<Inner>>) -> Box<FnOnce(CloseMsg)> {
     Box::new(move |msg: CloseMsg| {
         let mut inner = inner_rc.borrow_mut();
@@ -71,11 +78,44 @@ fn on_close(inner_rc: Rc<RefCell<Inner>>) -> Box<FnOnce(CloseMsg)> {
     })
 }
 
+/// Returns handler for bind on receive server message.
+fn on_message(
+    inner_rc: Rc<RefCell<Inner>>,
+) -> Box<FnMut(Result<ServerMsg, WasmErr>)> {
+    Box::new(move |msg: Result<ServerMsg, WasmErr>| {
+        let inner = inner_rc.borrow();
+        match msg {
+            Ok(ServerMsg::Pong(_num)) => {
+                // TODO: detect no pings
+                inner.pinger.set_pong_at(Date::now());
+            }
+            Ok(ServerMsg::Event(event)) => {
+                // TODO: many subs, filter messages by session
+                if let Some(sub) = inner.subs.iter().next() {
+                    if let Err(err) = sub.unbounded_send(event) {
+                        // TODO: receiver is gone, should delete
+                        //       this subs tx
+                        WasmErr::from(err).log_err();
+                    }
+                }
+            }
+            Err(err) => {
+                // TODO: protocol versions mismatch? should drop
+                //       connection if so
+                err.log_err();
+            }
+        }
+    })
+}
+
 impl RPCClient {
     pub fn new(token: String, ping_interval: i32) -> Self {
         Self(Inner::new(token, ping_interval))
     }
 
+    /// Creates new WebSocket connection to remote media server.
+    /// Start ['Pinger`] if connection success and bind handlers
+    /// on receive messages from server and close socket.
     pub fn init(&mut self) -> impl Future<Item = (), Error = WasmErr> {
         let inner = Rc::clone(&self.0);
         WebSocket::new(&self.0.borrow().token).and_then(
@@ -84,32 +124,7 @@ impl RPCClient {
                 inner.borrow_mut().pinger.start(Rc::clone(&socket))?;
 
                 let inner_rc = Rc::clone(&inner);
-                socket.on_message(move |msg: Result<ServerMsg, WasmErr>| {
-
-
-                    let inner = inner_rc.borrow();
-                    match msg {
-                        Ok(ServerMsg::Pong(_num)) => {
-                            // TODO: detect no pings
-                            inner.pinger.set_pong_at(Date::now());
-                        }
-                        Ok(ServerMsg::Event(event)) => {
-                            // TODO: many subs, filter messages by session
-                            if let Some(sub) = inner.subs.iter().next() {
-                                if let Err(err) = sub.unbounded_send(event) {
-                                    // TODO: receiver is gone, should delete
-                                    //       this subs tx
-                                    WasmErr::from(err).log_err();
-                                }
-                            }
-                        }
-                        Err(err) => {
-                            // TODO: protocol versions mismatch? should drop
-                            //       connection if so
-                            err.log_err();
-                        }
-                    }
-                })?;
+                socket.on_message(on_message(inner_rc))?;
 
                 let inner_rc = Rc::clone(&inner);
                 socket.on_close(on_close(inner_rc))?;
