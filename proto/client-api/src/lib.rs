17d3de23d7ddf05afe85cc2c7d613fa122faff2f
tyranron <tyranron@gmail.com>
2019-06-03 11:46:03 +0300

    Fix alphabetical order in 'medea-client-api-proto' crate

--- proto/client-api/src/lib.rs
+++ proto/client-api/src/lib.rs
@@ -2,8 +2,8 @@ use medea_macro::dispatchable;
 use serde::{de::Deserializer, ser::Serializer, Deserialize, Serialize};
 
 // TODO: should be properly shared between medea and jason
-#[cfg_attr(test, derive(PartialEq, Debug))]
 #[allow(dead_code)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 /// Message sent by `Media Server` to `Client`.
 pub enum ServerMsg {
     /// `pong` message that server answers with to WebSocket client in response
@@ -14,8 +14,8 @@ pub enum ServerMsg {
     Event(Event),
 }
 
-#[cfg_attr(test, derive(PartialEq, Debug))]
 #[allow(dead_code)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 /// Message from 'Client' to 'Media Server'.
 pub enum ClientMsg {
     /// `ping` message that WebSocket client is expected to send to the server
@@ -26,11 +26,11 @@ pub enum ClientMsg {
 }
 
 /// WebSocket message from Web Client to Media Server.
-#[cfg_attr(test, derive(PartialEq, Debug))]
+#[allow(dead_code)]
 #[cfg_attr(feature = "medea", derive(Deserialize))]
 #[cfg_attr(feature = "jason", derive(Serialize))]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 #[serde(tag = "command", content = "data")]
-#[allow(dead_code)]
 pub enum Command {
     /// Web Client sends SDP Offer.
     MakeSdpOffer { peer_id: u64, sdp_offer: String },
@@ -44,12 +44,12 @@ pub enum Command {
 }
 
 /// WebSocket message from Medea to Jason.
-#[dispatchable]
+#[allow(dead_code)]
 #[cfg_attr(feature = "medea", derive(Serialize))]
 #[cfg_attr(feature = "jason", derive(Deserialize))]
-#[cfg_attr(test, derive(PartialEq, Debug))]
+#[cfg_attr(test, derive(Debug, PartialEq))]
+#[dispatchable]
 #[serde(tag = "event", content = "data")]
-#[allow(dead_code)]
 pub enum Event {
     /// Media Server notifies Web Client about necessity of RTCPeerConnection
     /// creation.
@@ -75,7 +75,7 @@ pub enum Event {
 }
 
 /// Represents [`RtcIceCandidateInit`] object.
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub struct IceCandidate {
     pub candidate: String,
     pub sdp_m_line_index: Option<u16>,
@@ -85,7 +85,7 @@ pub struct IceCandidate {
 /// [`Track] with specified direction.
 #[cfg_attr(feature = "medea", derive(Serialize))]
 #[cfg_attr(feature = "jason", derive(Deserialize))]
-#[cfg_attr(test, derive(PartialEq, Debug))]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub struct Track {
     pub id: u64,
     pub direction: Direction,
@@ -95,7 +95,7 @@ pub struct Track {
 /// Direction of [`Track`].
 #[cfg_attr(feature = "medea", derive(Serialize))]
 #[cfg_attr(feature = "jason", derive(Deserialize))]
-#[cfg_attr(test, derive(PartialEq, Debug))]
+#[cfg_attr(test, derive(Debug, PartialEq))]
 pub enum Direction {
     Send { receivers: Vec<u64> },
     Recv { sender: u64 },
