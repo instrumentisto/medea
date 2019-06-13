44bc7e6511775b19a56834cc1383c4cc8c8e2f89
tyranron <tyranron@gmail.com>
2019-06-13 13:13:51 +0300

    Correct config defaults

--- src/conf/turn.rs
+++ src/conf/turn.rs
@@ -14,11 +14,11 @@ use smart_default::*;
 pub struct Turn {
     /// Database settings
     pub db: Db,
-    /// IP address STUN/TURN server. Defaults to `0.0.0.0`.
-    #[default(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)))]
+    /// IP address STUN/TURN server. Defaults to `127.0.0.1`.
+    #[default(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))]
     pub ip: IpAddr,
     /// Port to connect TURN server. Defaults to `3478`.
-    #[default(3478)]
+    #[default = 3478]
     pub port: u16,
     /// Username for authorize on TURN server.
     #[default(String::from("USER"))]
@@ -50,17 +50,17 @@ pub struct Db {
 #[derive(Clone, Debug, Deserialize, Serialize, SmartDefault)]
 #[serde(default)]
 pub struct Redis {
-    /// IP address Redis server. Defaults to `0.0.0.0`.
-    #[default(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)))]
+    /// IP address Redis server. Defaults to `127.0.0.1`.
+    #[default(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))]
     pub ip: IpAddr,
     /// Port to connect Redis server. Defaults to `6379`.
-    #[default(6379)]
+    #[default = 6379]
     pub port: u16,
     /// Password for authorize on Redis server.
     #[default(String::from("turn"))]
     pub pass: String,
     /// The database number to use. This is usually 0.
-    #[default(0)]
+    #[default = 0]
     pub db_number: i64,
     /// The duration to wait to start a connection before returning err.
     #[default(Duration::from_secs(5))]
