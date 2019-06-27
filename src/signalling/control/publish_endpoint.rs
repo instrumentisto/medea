be59ccf53ef21829aeaadf05b9f296d3fc83f120
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-06-27 17:39:00 +0300

    Add publish endpoint useful fns

--- src/signalling/control/publish_endpoint.rs
+++ src/signalling/control/publish_endpoint.rs
@@ -16,3 +16,35 @@ pub struct WebRtcPublishEndpoint {
     ice_user: Option<Rc<IceUser>>,
     peer_ids: HashSet<PeerId>
 }
+
+impl WebRtcPublishEndpoint {
+    pub fn add_peer_id(&mut self, peer_id: PeerId) {
+        self.peer_ids.insert(peer_id);
+    }
+
+    pub fn add_sink(&mut self, id: PlayerEndpointId) {
+        self.sinks.push(id);
+    }
+
+    pub fn owner(&self) -> &MemberId {
+        &self.owner
+    }
+
+    pub fn peer_ids(&self) -> &HashSet<PeerId> {
+        &self.peer_ids
+    }
+
+    pub fn reset(&mut self) {
+        self.peer_ids = HashSet::new();
+    }
+
+    pub fn remove_peer_id(&mut self, peer_id: &PeerId) -> bool {
+        self.peer_ids.remove(peer_id)
+    }
+
+    pub fn remove_peer_ids(&mut self, peer_ids: &[PeerId]) {
+        for peer_id in peer_ids {
+            self.remove_peer_id(peer_id)
+        }
+    }
+}
