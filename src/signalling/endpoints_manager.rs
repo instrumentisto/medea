c1319267c44f60513951f2a027c74fe7556f0a19
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-06-27 18:16:16 +0300

    Add basic useful fns to EndpointsManager

--- src/signalling/endpoints_manager.rs
+++ src/signalling/endpoints_manager.rs
@@ -7,11 +7,10 @@ use crate::api::control::MemberId;
 use super::control::play_endpoint::Id as PlayEndpointId;
 use super::control::publish_endpoint::Id as PublishEndpointId;
 use crate::media::IceUser;
+use medea_client_api_proto::IceServer;
 use crate::signalling::room::Room;
 use futures::Future;
 use actix::Context;
-use std::iter::Iterator;
-use std::iter::IntoIterator;
 
 #[derive(Debug)]
 pub struct EndpointsManager {
@@ -37,27 +36,43 @@ impl EndpointsManager {
         ice_users
     }
 
-//    pub fn drop_connections(&mut self, ctx: &mut Context<Room>) -> impl Future<Item = (), Error = ()> {
-//        let remove_ice_users = Box::new({
-//            let mut ice_users = HashMap::new();
-//            std::mem::swap(&mut self.ice_users, &mut ice_users);
-//            let ice_users: Vec<Rc<IceUser>> = ice_users.into_iter().map(|(_, ice_user)| ice_user).collect();
-//
-//        });
-////        let remove_ice_users = Box::new({
-////            let mut room_users = Vec::with_capacity(self.participants.len());
-////
-////            self.participants.iter().for_each(|(_, data)| {
-////                if let Some(ice_user) = data.take_ice_user() {
-////                    room_users.push(ice_user);
-////                }
-////            });
-////            self.turn
-////                .delete(room_users)
-////                .map_err(|err| error!("Error removing IceUsers {:?}", err))
-////        });
-////        close_fut.push(remove_ice_users);
-////
-////        join_all(close_fut).map(|_| ())
-//    }
+    pub fn take_ice_user_by_member_id(&mut self, member_id: &MemberId) -> Option<IceUser> {
+        self.ice_users.remove(member_id)
+    }
+
+    pub fn replace_ice_user(&mut self, member_id: MemberId, mut new_ice_user: Rc<IceUser>) -> Option<Rc<IceUser>> {
+        self.ice_users.insert(member_id.clone(), new_ice_user)
+    }
+
+    pub fn peers_removed(&mut self, peer_ids: &[PeerId]) {
+        self.publishers
+            .iter()
+            .for_each(|(_, p)| p.remove_peer_ids(peer_ids));
+
+        self.receivers
+            .iter()
+            .filter_map(|(_, p)| p.peer_id().map(|id| (id, p)))
+            .filter(|(id, _)| peer_ids.contains(&id))
+            .for_each(|(_, p)| p.reset());
+    }
+
+    pub fn get_servers_list_by_member_id(&self, member_id: &MemberId) -> Option<Vec<IceServer>> {
+        self.ice_users.get(member_id).as_ref().map(IceUser::servers_list)
+    }
+
+    pub fn insert_receiver(&mut self, id: PlayEndpointId, receiver: WebRtcPlayEndpoint) {
+        self.receivers.insert(id, receiver);
+    }
+
+    pub fn insert_publisher(&mut self, id: PublishEndpointId, publisher: WebRtcPublishEndpoint) {
+        self.publishers.insert(id, publisher);
+    }
+
+    pub fn get_publisher_by_id(&self, id: &PublishEndpointId) -> &WebRtcPublishEndpoint {
+        self.publishers.get(id)
+    }
+
+    pub fn get_receiver_by_id(&self, id: &PlayEndpointId) -> &WebRtcPlayEndpoint {
+        self.receivers.get(id)
+    }
 }
