0e4c449889398e93cd6a99d942c521725d306752
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-07-04 17:52:50 +0300

    Add SrcUri protobuf implementation

--- src/api/control/protobuf/webrtc_play_endpoint.rs
+++ src/api/control/protobuf/webrtc_play_endpoint.rs
@@ -14,15 +14,55 @@ pub struct GrpcWebRtcPlayEndpoint(pub WebRtcPlayEndpointDto);
 impl WebRtcPlayEndpoint for GrpcWebRtcPlayEndpoint {
     fn src(&self) -> SerdeSrcUri {
         if self.0.has_src() {
-            let _src = self.0.get_src();
-            SerdeSrcUri {
-                endpoint_id: WebRtcPublishId("".to_string()),
-                member_id: MemberId("".to_string()),
-                room_id: RoomId("".to_string()),
-            }
+            let src = self.0.get_src();
+            parse_src_uri(src)
         } else {
             // TODO: do something with it.
             unimplemented!("TODO")
         }
+    }
+}
+
+// TODO: use already done implementation from serde DTO
+//       share this with serde deseralizer.
+fn parse_src_uri(value: &str) -> SerdeSrcUri {
+    let protocol_name: String = value.chars().take(8).collect();
+    if protocol_name != "local://" {
+        panic!()
+    }
+
+    let uri_body = value.chars().skip(8).collect::<String>();
+    let mut uri_body_splitted: Vec<&str> =
+        uri_body.rsplit('/').collect();
+    let uri_body_splitted_len = uri_body_splitted.len();
+    if uri_body_splitted_len != 3 {
+        let error_msg = if uri_body_splitted_len == 0 {
+            "room_id, member_id, endpoint_id"
+        } else if uri_body_splitted_len == 1 {
+            "member_id, endpoint_id"
+        } else if uri_body_splitted_len == 2 {
+            "endpoint_id"
+        } else {
+            panic!()
+        };
+        panic!()
+    }
+    let room_id = uri_body_splitted.pop().unwrap().to_string();
+    if room_id.is_empty() {
+        panic!()
+    }
+    let member_id = uri_body_splitted.pop().unwrap().to_string();
+    if member_id.is_empty() {
+        panic!()
+    }
+    let endpoint_id = uri_body_splitted.pop().unwrap().to_string();
+    if endpoint_id.is_empty() {
+        panic!()
+    }
+
+    SerdeSrcUri {
+        room_id: RoomId(room_id),
+        member_id: MemberId(member_id),
+        endpoint_id: WebRtcPublishId(endpoint_id),
     }
 }
