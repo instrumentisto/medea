0e96f96d06c1c1fdba538e5735b56ba96ebc61cc
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-07-05 12:37:52 +0300

    gRPC client for creating new room

---
+++ src/bin/client.rs
@@ -0,0 +1,48 @@
+use medea::api::grpc::protos::control_grpc::ControlApiClient;
+use medea::api::grpc::protos::control::{CreateRequest, Room, Member, WebRtcPlayEndpoint, WebRtcPublishEndpoint, Member_Element, WebRtcPublishEndpoint_P2P, Room_Element};
+use std::sync::Arc;
+use grpcio::{EnvBuilder, ChannelBuilder};
+use std::collections::HashMap;
+
+fn main() {
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("localhost:50051");
+    let client = ControlApiClient::new(ch);
+
+    let mut req = CreateRequest::new();
+    let mut room = Room::new();
+    let mut publisher = Member::new();
+    let mut responder = Member::new();
+    let mut play_endpoint = WebRtcPlayEndpoint::new();
+    let mut publish_endpoint = WebRtcPublishEndpoint::new();
+
+    play_endpoint.set_src("local://grpc-test/publisher".to_string());
+    let mut play_endpoint_element = Member_Element::new();
+    play_endpoint_element.set_webrtc_play(play_endpoint);
+    let mut responder_pipeline = HashMap::new();
+    responder_pipeline.insert("play".to_string(), play_endpoint_element);
+    responder.set_pipeline(responder_pipeline);
+    responder.set_credentials("test".to_string());
+
+    publish_endpoint.set_p2p(WebRtcPublishEndpoint_P2P::ALWAYS);
+    let mut publish_endpoint_element = Member_Element::new();
+    publish_endpoint_element.set_webrtc_pub(publish_endpoint);
+    let mut publisher_pipeline = HashMap::new();
+    publisher_pipeline.insert("publish".to_string(), publish_endpoint_element);
+    publisher.set_pipeline(publisher_pipeline);
+    publisher.set_credentials("test".to_string());
+
+    let mut publisher_member_element = Room_Element::new();
+    publisher_member_element.set_member(publisher);
+    let mut responder_member_element = Room_Element::new();
+    responder_member_element.set_member(responder);
+    let mut room_pipeline = HashMap::new();
+    room_pipeline.insert("publisher".to_string(), publisher_member_element);
+    room_pipeline.insert("responder".to_string(), responder_member_element);
+    room.set_pipeline(room_pipeline);
+    req.set_room(room);
+    req.set_id("grpc-test".to_string());
+
+    let reply = client.create(&req).expect("rpc");
+    println!("Receiver: {:?}", reply.get_sid());
+}
