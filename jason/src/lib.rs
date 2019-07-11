e155905a113a1c682b7f73a0e3da9bd251c4130b
alexlapa <lapa.alex@ex.ua>
2019-07-11 20:53:11 +0300

    remove cfg_if and wee_alloc

--- jason/src/lib.rs
+++ jason/src/lib.rs
@@ -1,5 +1,3 @@
-use cfg_if::cfg_if;
-
 mod api;
 mod rpc;
 mod utils;
@@ -7,22 +5,8 @@ mod utils;
 #[doc(inline)]
 pub use self::api::{Jason, RoomHandle};
 
-cfg_if! {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function to get better error messages if we ever panic.
-    if #[cfg(feature = "console_error_panic_hook")] {
-        pub use console_error_panic_hook::set_once as set_panic_hook;
-    } else {
-        #[inline]
-        pub fn set_panic_hook() {}
-    }
-}
+#[cfg(feature = "console_error_panic_hook")]
+pub use console_error_panic_hook::set_once as set_panic_hook;
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
+#[cfg(not(feature = "console_error_panic_hook"))]
+pub fn set_panic_hook() {}
