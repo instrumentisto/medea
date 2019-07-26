7222645e90f5f605b08f4b265f8d48bedb8b5736
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-07-26 17:50:21 +0300

    Add docs

--- e2e-tests/src/main.rs
+++ e2e-tests/src/main.rs
@@ -1,3 +1,4 @@
+/// E2e tests runner.
 pub mod mocha_result;
 pub mod test_runner;
 
@@ -13,19 +14,27 @@ use futures::Future;
 
 use crate::test_runner::TestRunner;
 
+/// HTTP endpoint which return requested file from this dir.
+/// Used for loading tests.
 #[allow(clippy::needless_pass_by_value)]
 fn index(req: HttpRequest) -> HttpResult<NamedFile> {
     let path: PathBuf = req.match_info().query("filename").parse().unwrap();
     Ok(NamedFile::open(path)?)
 }
 
-fn run_http_server(addr: &str) -> Server {
+/// Start server which serve files from this dir.
+///
+/// WebDriver's browser will go into test files from this server.
+///
+/// This is needed because restriction for type=module scripts.
+fn run_test_files_server(addr: &str) -> Server {
     HttpServer::new(|| App::new().route("{filename:.*}", web::get().to(index)))
         .bind(addr)
         .unwrap()
         .start()
 }
 
+/// Returns [`PathBuf`] to test/test dir from clap's [`ArgMatches`].
 fn get_path_to_tests_from_args(opts: &ArgMatches) -> PathBuf {
     let path_to_tests = opts.value_of("specs_path").unwrap();
     let path_to_tests = PathBuf::from(path_to_tests);
@@ -63,7 +72,7 @@ fn main() {
 
     actix::run(|| {
         let server =
-            run_http_server(opts.value_of("tests_files_addr").unwrap());
+            run_test_files_server(opts.value_of("tests_files_addr").unwrap());
         let path_to_tests = get_path_to_tests_from_args(&opts);
         TestRunner::run(path_to_tests, &opts)
             .and_then(move |_| server.stop(true))
--- e2e-tests/src/mocha_result.rs
+++ e2e-tests/src/mocha_result.rs
@@ -1,16 +1,28 @@
+/// Objects which will be returned from mocha tests.
 use serde::Deserialize;
 use std::fmt;
 use yansi::Paint;
 
+/// Results of mocha tests.
+///
+/// This struct will be parsed from mocha's JSON reporters string.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TestResults {
+    /// Summary stats of all tests.
     pub stats: TestStats,
+
+    /// Collection of all failed tests.
     pub failures: Vec<FailureTestResult>,
+
+    /// Collection of all success tests.
     pub passes: Vec<SuccessTestResult>,
 }
 
 impl TestResults {
+    /// Check that results of mocha tests has error.
+    ///
+    /// Returns `true` if failures vector not empty.
     pub fn is_has_error(&self) -> bool {
         !self.failures.is_empty()
     }
@@ -41,31 +53,58 @@ impl fmt::Display for TestResults {
     }
 }
 
+/// Summary stats of mocha tests.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TestStats {
+    /// Count of tested suites.
     pub suites: i32,
+
+    /// Count of tested tests.
     pub tests: i32,
+
+    /// Count of passed tests.
     pub passes: i32,
+
+    /// Count of pending tests.
+    /// This is __useless__ field because in our case this count always zero.
     pub pending: i32,
+
+    /// Count of failed tests.
     pub failures: i32,
+
+    /// Time when tests started.
     pub start: String,
+
+    /// Time when tests completed.
     pub end: String,
+
+    /// Total duration of all tests.
     pub duration: u32,
 }
 
+/// Test which failed.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TestError {
+    /// Error message.
     pub message: String,
+
+    /// Stacktrace from JS side where exception thrown.
     pub stack: String,
 }
 
+/// Test which success.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SuccessTestResult {
+    /// Title of test.
     pub title: String,
+
+    /// Title of test with context.
     pub full_title: String,
+
+    /// Duration of test.
     pub duration: u32,
 }
 
@@ -83,12 +122,20 @@ impl fmt::Display for SuccessTestResult {
     }
 }
 
+/// Test which failed
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FailureTestResult {
+    /// Title of test.
     pub title: String,
+
+    /// Title of test with context.
     pub full_title: String,
+
+    /// How much retries happened.
     pub current_retry: i32,
+
+    /// What error happened.
     pub err: TestError,
 }
 
