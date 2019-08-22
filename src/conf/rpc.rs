9cc83319c28d11a31048c474eedee02fba8c1a6a
tyranron <tyranron@gmail.com>
2019-08-22 11:15:15 +0300

    Create 0.1.0 release of 'medea' crate
    - exclude unnecessary files from crate distribution
    - fix license name in Cargo manifest
    - correct DockerHub 'post_push' hook to omit tagging non-stable versions
    - replace 'serde-humantime' fork with 'humantime-serde' crate
    
    Additionally:
    - disable caching for 'rustfmt' job on Travis CI pipeline

--- src/conf/rpc.rs
+++ src/conf/rpc.rs
@@ -11,12 +11,12 @@ pub struct Rpc {
     /// Duration, after which remote RPC client will be considered idle if no
     /// heartbeat messages received. Defaults to `10s`.
     #[default(Duration::from_secs(10))]
-    #[serde(with = "serde_humantime")]
+    #[serde(with = "humantime_serde")]
     pub idle_timeout: Duration,
 
     /// Duration, after which the server deletes the client session if
     /// the remote RPC client does not reconnect after it is idle.
     #[default(Duration::from_secs(10))]
-    #[serde(with = "serde_humantime")]
+    #[serde(with = "humantime_serde")]
     pub reconnect_timeout: Duration,
 }
