5262d4e024172f3e6711b2d3f0d51d8efada646a
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-08-23 20:14:35 +0300

    More clean travis_install.sh [run ci]

--- travis_install.sh
+++ travis_install.sh
@@ -1,5 +1,20 @@
-# TODO: use array
-if [[ $TRAVIS_BUILD_STAGE_NAME == "Build" ]] || [[ $TRAVIS_JOB_NAME == "Clippy" ]] || [[ $TRAVIS_JOB_NAME == "unit (stable)" ]] ; then
+array_contains () {
+    local array="$1[@]"
+    local seeking=$2
+    local in=1
+    for element in "${!array}"; do
+        if [[ $element == $seeking ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
+}
+
+protoc_jobs=("Clippy" "unit (stable)")
+protoc_stages=("build")
+
+if array_contains protoc_jobs "$TRAVIS_JOB_NAME" || array_contains protoc_stages "$TRAVIS_BUILD_STAGE_NAME" ; then
   echo "Installing protoc"
   PROTOBUF_VERSION=3.3.0
   PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
