a40aefd7ce568ede3e8224ea720616191b0e4518
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-08-24 10:02:30 +0300

    Add DONT_INSTALL_PROTOC env for Travis [run ci]

--- travis_install.sh
+++ travis_install.sh
@@ -1,20 +1,4 @@
-array_contains () {
-    local array="$1[@]"
-    local seeking=$2
-    local in=1
-    for element in "${!array}"; do
-        if [[ $element == $seeking ]]; then
-            in=0
-            break
-        fi
-    done
-    return $in
-}
-
-protoc_jobs=("Clippy" "unit (stable)")
-protoc_stages=("build")
-
-if array_contains protoc_jobs "$TRAVIS_JOB_NAME" || array_contains protoc_stages "$TRAVIS_BUILD_STAGE_NAME" ; then
+if [[ ! $DONT_INSTALL_PROTOC ]] ; then
   echo "Installing protoc"
   PROTOBUF_VERSION=3.3.0
   PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
