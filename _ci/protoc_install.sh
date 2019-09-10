bb832cf32b553dca50100cfb858dece4a4ff12e8
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-09-10 12:16:01 +0300

    Use _ci/install_protoc.sh in Dockerfile

--- _ci/protoc_install.sh
+++ _ci/protoc_install.sh
@@ -4,9 +4,10 @@ if [[ ! $DONT_INSTALL_PROTOC ]] ; then
   echo "Installing protoc"
   PROTOBUF_VERSION=3.3.0
   PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-  pushd /home/travis || exit
+  pushd "${HOME}" || exit
   wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
   unzip ${PROTOC_FILENAME}
-  bin/protoc --version
+  cp bin/protoc /usr/bin
+  protoc --version
   popd || exit
 fi
