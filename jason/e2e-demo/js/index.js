2f48dc30bccd73412a448a77f40ece4b96f33241
tyranron <tyranron@gmail.com>
2019-09-13 15:43:27 +0300

    Correct JS and HTML code style

--- jason/e2e-demo/js/index.js
+++ jason/e2e-demo/js/index.js
@@ -1,55 +1,50 @@
-
 async function init_participant(wasm, token, frame) {
-    let toggle_audio = $(frame).find("input[name=toggle-audio]");
-    let toggle_video = $(frame).find("input[name=toggle-video]");
-    let local_video = $(frame).find("video[name=local-video]")[0];
-    let remote_video = $(frame).find("video[name=remote-video]")[0];
+  let toggle_audio = $(frame).find("input[name=toggle-audio]");
+  let toggle_video = $(frame).find("input[name=toggle-video]");
+  let local_video = $(frame).find("video[name=local-video]")[0];
+  let remote_video = $(frame).find("video[name=remote-video]")[0];
 
-    let participant = new wasm.Jason();
-    let room = await participant.join_room(token);
+  let participant = new wasm.Jason();
+  let room = await participant.join_room(token);
 
-    toggle_audio.change(function() {
-        if($(this).is(":checked")) {
-            room.unmute_audio();
-        } else {
-            room.mute_audio();
-        }
+  toggle_audio.change(function() {
+    if($(this).is(":checked")) {
+      room.unmute_audio();
+    } else {
+      room.mute_audio();
+    }
+  });
+
+  toggle_video.change(function() {
+    if($(this).is(":checked")) {
+      room.unmute_video();
+    } else {
+      room.mute_video();
+    }
+  });
+
+  room.on_new_connection(function (connection) {
+    connection.on_remote_stream(function (stream) {
+      remote_video.srcObject = stream.get_media_stream();
+      remote_video.play();
     });
+  });
 
-    toggle_video.change(function() {
-        if($(this).is(":checked")) {
-            room.unmute_video();
-        } else {
-            room.mute_video();
-        }
-    });
+  participant.on_local_stream(function (stream, error) {
+    if (stream) {
+      local_video.srcObject = stream.get_media_stream();
+      local_video.play();
+    } else {
+      console.log(error);
+    }
+  });
 
-    room.on_new_connection(function (connection) {
-        connection.on_remote_stream(function (stream) {
-            remote_video.srcObject = stream.get_media_stream();
-            remote_video.play();
-        });
-    });
-
-    participant.on_local_stream(function (stream, error) {
-        if (stream) {
-            local_video.srcObject = stream.get_media_stream();
-            local_video.play();
-        } else {
-            console.log(error);
-        }
-    });
-
-    return room;
+  return room;
 }
 
 window.onload = async function () {
+  const wasm = await import("../../pkg");
 
-    const wasm = await import("../../pkg");
-
-    await init_participant(wasm, "ws://localhost:8080/ws/pub-pub-video-call/caller/test", "#caller");
-    await init_participant(wasm, "ws://localhost:8080/ws/pub-pub-video-call/responder/test", "#responder");
-
-
+  await init_participant(wasm, "ws://localhost:8080/ws/pub-pub-video-call/caller/test", "#caller");
+  await init_participant(wasm, "ws://localhost:8080/ws/pub-pub-video-call/responder/test", "#responder");
 };
-
