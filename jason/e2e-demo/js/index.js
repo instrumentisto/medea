a74d61fe870bf686117fad4cfb7c4cb2e478c6d3
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-09-18 15:03:49 +0300

    Remove jquery from e2e-demo, add mute state restoring

--- jason/e2e-demo/js/index.js
+++ jason/e2e-demo/js/index.js
@@ -1,39 +1,53 @@
-async function init_participant(wasm, token, frame) {
-  let toggle_audio = $(frame).find("input[name=toggle-audio]");
-  let toggle_video = $(frame).find("input[name=toggle-video]");
-  let local_video = $(frame).find("video[name=local-video]")[0];
-  let remote_video = $(frame).find("video[name=remote-video]")[0];
+function audioToggle(room, toggle) {
+  if(toggle.checked) {
+    room.unmute_audio();
+  } else {
+    room.mute_audio();
+  }
+}
+
+function videoToggle(room, toggle) {
+  if(toggle.checked) {
+    room.unmute_video();
+  } else {
+    room.mute_video();
+  }
+}
+
+async function init_participant(wasm, token, frameSelector) {
+  let frame = document.querySelector(frameSelector);
+
+  let toggleAudio = frame.querySelector("input[name=toggle-audio]");
+  let toggleVideo = frame.querySelector("input[name=toggle-video]");
+  let localVideo = frame.querySelector("video[class=local-video]");
+  let remoteVideo = frame.querySelector("video[class=remote-video]");
 
   let participant = new wasm.Jason();
   let room = await participant.join_room(token);
 
-  toggle_audio.change(function() {
-    if($(this).is(":checked")) {
-      room.unmute_audio();
-    } else {
-      room.mute_audio();
-    }
+  // Restore mute state after page refresh.
+  audioToggle(room, toggleAudio);
+  videoToggle(room, toggleVideo);
+
+  toggleAudio.addEventListener('change', (t) => {
+      audioToggle(room, t.target);
   });
 
-  toggle_video.change(function() {
-    if($(this).is(":checked")) {
-      room.unmute_video();
-    } else {
-      room.mute_video();
-    }
+  toggleVideo.addEventListener('change', (t) => {
+      videoToggle(room, t.target);
   });
 
   room.on_new_connection(function (connection) {
     connection.on_remote_stream(function (stream) {
-      remote_video.srcObject = stream.get_media_stream();
-      remote_video.play();
+      remoteVideo.srcObject = stream.get_media_stream();
+      remoteVideo.play();
     });
   });
 
   participant.on_local_stream(function (stream, error) {
     if (stream) {
-      local_video.srcObject = stream.get_media_stream();
-      local_video.play();
+      localVideo.srcObject = stream.get_media_stream();
+      localVideo.play();
     } else {
       console.log(error);
     }
