5e6c2a8bacf926d133606b90d6df30c8550df9e3
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-10-29 14:48:34 +0300

    Add deserialization of callback URL and test for it

--- src/api/control/callback/callback_url.rs
+++ src/api/control/callback/callback_url.rs
@@ -1,4 +1,6 @@
 use derive_more::Display;
+use std::convert::TryFrom;
+use url::{ParseError, Url};
 
 #[derive(Clone, Display, Debug, Eq, PartialEq, Hash)]
 pub struct GrpcCallbackUrl(String);
@@ -6,4 +8,125 @@ pub struct GrpcCallbackUrl(String);
 #[derive(Clone, Display, Debug, Eq, PartialEq, Hash)]
 pub enum CallbackUrl {
     Grpc(GrpcCallbackUrl),
+}
+
+#[derive(Debug, Display)]
+pub enum CallbackUrlParseError {
+    #[display(fmt = "{:?}", _0)]
+    UrlParseErr(ParseError),
+
+    #[display(fmt = "Missing host.")]
+    MissingHost,
+
+    #[display(fmt = "Unsupported URL scheme.")]
+    UnsupportedScheme,
+}
+
+impl From<ParseError> for CallbackUrlParseError {
+    fn from(err: ParseError) -> Self {
+        Self::UrlParseErr(err)
+    }
+}
+
+impl TryFrom<String> for CallbackUrl {
+    type Error = CallbackUrlParseError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let url = Url::parse(&value)?;
+        let url_scheme = url.scheme();
+        let host = url
+            .host()
+            .ok_or_else(|| CallbackUrlParseError::MissingHost)?;
+        let host = if let Some(port) = url.port() {
+            format!("{}:{}", host, port)
+        } else {
+            host.to_string()
+        };
+
+        match url_scheme {
+            "grpc" => Ok(CallbackUrl::Grpc(GrpcCallbackUrl(host))),
+            _ => Err(CallbackUrlParseError::UnsupportedScheme),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn successful_parse_grpc_url() {
+        for (url, expected_callback_url) in &[
+            ("grpc://127.0.0.1:9090", "127.0.0.1:9090"),
+            ("grpc://example.com:9090", "example.com:9090"),
+            ("grpc://example.com", "example.com"),
+            ("grpc://127.0.0.1", "127.0.0.1"),
+        ] {
+            let callback_url = CallbackUrl::try_from(url.to_string()).unwrap();
+            match callback_url {
+                CallbackUrl::Grpc(grpc_callback_url) => {
+                    assert_eq!(
+                        grpc_callback_url.to_string(),
+                        expected_callback_url.to_string()
+                    );
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn error_on_unsupported_scheme() {
+        for url in &[
+            "asdf://example.com:9090",
+            "asdf://127.0.0.1",
+            "asdf://127.0.0.1:9090",
+        ] {
+            let res = if let Err(e) = CallbackUrl::try_from(url.to_string()) {
+                e
+            } else {
+                unreachable!(
+                    "Unreachable successful result of parsing. {}",
+                    url
+                );
+            };
+            match res {
+                CallbackUrlParseError::UnsupportedScheme => {}
+                _ => {
+                    unreachable!("Unreachable error (URL = {}): {:?}", url, res)
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn error_on_url_without_scheme() {
+        for url in &[
+            "127.0.0.1",
+            "127.0.0.1:9090",
+            "example.com",
+            "example.com:9090",
+        ] {
+            let res = if let Err(e) = CallbackUrl::try_from(url.to_string()) {
+                e
+            } else {
+                unreachable!(
+                    "Unreachable successful result of parsing. {}",
+                    url
+                );
+            };
+            match res {
+                CallbackUrlParseError::UrlParseErr(e) => match e {
+                    ParseError::RelativeUrlWithoutBase => {}
+                    _ => unreachable!(
+                        "Unreachable ParseError [URL = {}]: {:?}.",
+                        url, e
+                    ),
+                },
+                CallbackUrlParseError::MissingHost => {}
+                _ => {
+                    unreachable!("Unreachable error [URL = {}]: {:?}", url, res)
+                }
+            }
+        }
+    }
 }
