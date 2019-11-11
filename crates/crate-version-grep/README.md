d4a02d45181cd535923e2ee79ddcb4875d6d87c5
Semen Evdokimov <evdokimovs@protonmail.ch>
2019-11-11 13:29:16 +0300

    Add README, docs, minor refactoring

---
+++ crates/crate-version-grep/README.md
@@ -0,0 +1,20 @@
+crate-version-grep
+==================
+
+CLI application for getting current locked version of crate from `Cargo.lock` file.
+
+
+
+
+## Usage
+
+```bash
+$ crate-version-grep {{ CRATE NAME }}
+```
+
+
+### Example
+
+```bash
+$ crate-version-grep wasm-bingen # -> 0.2.53
+```
--- crates/crate-version-grep/src/main.rs
+++ crates/crate-version-grep/src/main.rs
@@ -1,16 +1,28 @@
+//! CLI application for getting current locked version of crate from
+//! `Cargo.lock` file.
+//!
+//! # Usage
+//!
+//! ```bash
+//! $ crate-version-grep {{ CRATE NAME }}
+//! ```
+//!
+//! ## Example
+//!
+//! ```bash
+//! $ crate-version-grep wasm-bingen # -> 0.2.53
+//! ```
+
 use std::env;
 
 use cargo_lock::lockfile::Lockfile;
 
 fn main() {
-    let crate_name = env::args()
-        .skip(1)
-        .next()
-        .expect("Crate name not provided!");
+    let crate_name = env::args().nth(1).expect("Crate name not provided!");
     let lockfile = Lockfile::load("Cargo.lock").unwrap();
 
     for package in lockfile.packages {
-        if package.name.as_str() == &crate_name {
+        if package.name.as_str() == crate_name {
             println!("{}", package.version);
             return;
         }
