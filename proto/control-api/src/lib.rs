272b79ec065f09e6b615e4b3dc239474ec97756c
tyranron <tyranron@gmail.com>
2020-01-14 16:09:29 +0100

    Correct control-api proto [skip ci]

--- proto/control-api/src/lib.rs
+++ proto/control-api/src/lib.rs
@@ -3,8 +3,6 @@
 //! [Medea]: https://github.com/instrumentisto/medea
 //! [Control API]: https://tinyurl.com/yxsqplq7
 
-#![allow(clippy::must_use_candidate)]
-
 #[rustfmt::skip]
 #[cfg(feature = "grpc")]
 pub mod grpc;
