c21c80e94164a120a174dc726c88119beb91ce47
Semen Evdokimov <evdokimovs@protonmail.ch>
2020-03-02 19:20:11 +0300

    Add basic metrics

--- jason/src/peer/stats/mod.rs
+++ jason/src/peer/stats/mod.rs
@@ -14,10 +14,12 @@
 
 use std::convert::TryFrom;
 
+use serde::Deserialize;
 use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::RtcStats as SysRtcStats;
 
-use crate::utils::get_property_by_name;
+use crate::utils::{console_error, get_property_by_name};
+use futures::future::Remote;
 
 struct RtcStatsReportEntry(js_sys::JsString, SysRtcStats);
 
@@ -87,11 +89,15 @@ impl From<&JsValue> for RtcStats {
             let stat = RtcStatsReportEntry::try_from(stat).unwrap();
             let stat = RtcStatsType::try_from(&stat.1).unwrap();
 
+            console_error(format!("Stat: {:?}", stat));
+
             match stat {
                 RtcStatsType::CandidatePair(pair) => {
-                    if pair.kind.nominated {
+                    if pair.kind.nominated.unwrap_or_default() {
                         ice_pairs.push(pair);
                     }
+                }
+                RtcStatsType::OutboundRtp(stat) => {
                 }
                 RtcStatsType::Unknown(unknown_stat) => {
                     // print error
@@ -112,8 +118,8 @@ impl From<&JsValue> for RtcStats {
 #[derive(Debug)]
 enum RtcStatsType {
     Codec,
-    InboundRtp,
-    OutboundRtp,
+    InboundRtp(RtcStat<InboundRtcStats>),
+    OutboundRtp(RtcStat<OutboundRtcStats>),
     RemoteInboundRtp,
     RemoteOutboundRtp,
     MediaSoure,
@@ -121,15 +127,15 @@ enum RtcStatsType {
     PeerConnection,
     DataChannel,
     Stream,
-    Track,
+    Track(RtcStat<TrackStat>),
     Transceiver,
     Sender,
     Receiver,
     Transport,
     SctpTransport,
     CandidatePair(RtcStat<RtcIceCandidatePairStats>),
-    LocalCandidate,
-    RemoteCandidate,
+    LocalCandidate(RtcStat<LocalCandidateStat>),
+    RemoteCandidate(RtcStat<RemoteCandidateStat>),
     Certificate,
     IceServer,
     Unknown(String),
@@ -152,16 +158,24 @@ impl TryFrom<&SysRtcStats> for RtcStatsType {
 
         let kind = match kind.as_ref() {
             "codec" => Codec,
-            "local-candidate" => LocalCandidate,
-            "remote-candidate" => RemoteCandidate,
-            "track" => Track,
+            "local-candidate" => LocalCandidate(RtcStat::new(id, timestamp, LocalCandidateStat::from(val))),
+            "remote-candidate" => RemoteCandidate(RtcStat::new(id, timestamp, RemoteCandidateStat::from(val))),
+            "track" => Track(RtcStat::new(id, timestamp, TrackStat::from(val))),
             "candidate-pair" => CandidatePair(RtcStat::new(
                 id,
                 timestamp,
                 RtcIceCandidatePairStats::from(val),
             )),
-            "inbound-rtp" => InboundRtp,
-            "outbound-rtp" => OutboundRtp,
+            "inbound-rtp" => InboundRtp(RtcStat::new(
+                id,
+                timestamp,
+                InboundRtcStats::from(val),
+            )),
+            "outbound-rtp" => OutboundRtp(RtcStat::new(
+                id,
+                timestamp,
+                OutboundRtcStats::from(val),
+            )),
             "remote-inbound-rtp" => RemoteInboundRtp,
             "remote-outbound-rtp" => RemoteOutboundRtp,
             "media-source" => MediaSoure,
@@ -184,86 +198,37 @@ impl TryFrom<&SysRtcStats> for RtcStatsType {
 }
 
 // https://www.w3.org/TR/webrtc-stats/#candidatepair-dict*
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct RtcIceCandidatePairStats {
-    state: IceCandidatePairState,
-    nominated: bool,
-    writable: bool,
-    bytes_sent: u64,
-    bytes_received: u64,
-    total_round_trip_time: f64,
+    state: Option<IceCandidatePairState>,
+    nominated: Option<bool>,
+    writable: Option<bool>,
+    bytes_sent: Option<u64>,
+    bytes_received: Option<u64>,
+    total_round_trip_time: Option<f64>,
     current_round_trip_time: Option<f64>,
     available_outgoing_bitrate: Option<u64>,
 }
 
 impl From<&SysRtcStats> for RtcIceCandidatePairStats {
     fn from(val: &SysRtcStats) -> Self {
-        let state =
-            get_property_by_name(&val, "state", |val| val.as_string()).unwrap();
-        let nominated =
-            get_property_by_name(&val, "nominated", |val| val.as_bool())
-                .unwrap();
-        let writable =
-            get_property_by_name(&val, "writable", |val| val.as_bool())
-                .unwrap();
-        let bytes_sent = get_property_by_name(&val, "bytesSent", |val| {
-            val.as_f64()
-        })
-        .unwrap() as u64;
-        let bytes_received =
-            get_property_by_name(&val, "bytesReceived", |val| val.as_f64())
-                .unwrap() as u64;
-        let total_round_trip_time =
-            get_property_by_name(&val, "totalRoundTripTime", |val| {
-                val.as_f64()
-            })
-            .unwrap();
-        let current_round_trip_time =
-            get_property_by_name(&val, "currentRoundTripTime", |val| {
-                val.as_f64()
-            });
-        let available_outgoing_bitrate =
-            get_property_by_name(&val, "availableOutgoingBitrate", |val| {
-                val.as_f64()
-            })
-            .map(|val| val as u64);
-
-        RtcIceCandidatePairStats {
-            state: IceCandidatePairState::from(state.as_ref()),
-            nominated,
-            writable,
-            bytes_sent,
-            bytes_received,
-            total_round_trip_time,
-            current_round_trip_time,
-            available_outgoing_bitrate,
-        }
+        JsValue::from(val).into_serde().unwrap()
     }
 }
 
 // https://www.w3.org/TR/webrtc-stats/#rtcstatsicecandidatepairstate-enum
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 enum IceCandidatePairState {
     Frozen,
     Waiting,
     Inprogress,
     Failed,
     Succeeded,
-    Unknown(String),
-}
-
-impl From<&str> for IceCandidatePairState {
-    fn from(state: &str) -> Self {
-        use IceCandidatePairState::*;
-        match state {
-            "waiting" => Waiting,
-            "succeeded" => Succeeded,
-            "in-progress" => Inprogress,
-            "frozen" => Frozen,
-            "failed" => Failed,
-            _ => Unknown(state.to_owned()),
-        }
-    }
+    // TODO: make it Unknown(String) with unknown state inside.
+    #[serde(other)]
+    Unknown,
 }
 
 // https://www.w3.org/TR/webrtc-stats/#inboundrtpstats-dict*
@@ -274,4 +239,116 @@ struct RtcInboundRtpStreamStats {
     packets_received: u64,
     packets_lost: u64,
     jitter: f64,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct TrackStat {
+    #[serde(rename = "trackIdentifier")]
+    track_id: String,
+    remote_source: Option<bool>,
+    ended: bool,
+    detached: bool,
+    kind: String,
+    media_source_id: Option<String>,
+    jitter_buffer_delay: Option<f64>,
+    jitter_buffer_emitted_count: Option<f64>,
+    frame_width: Option<u64>,
+    frames_received: Option<f64>,
+    frames_decoded: Option<f64>,
+    frames_dropped: Option<f64>,
+}
+
+impl From<&SysRtcStats> for TrackStat {
+    fn from(val: &SysRtcStats) -> Self {
+        JsValue::from(val).into_serde().unwrap()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct InboundRtcStats {
+    jitter: Option<f64>,
+    kind: Option<String>,
+    media_type: Option<String>,
+    nack_count: Option<u32>,
+    packets_loss: Option<u32>,
+    packets_received: Option<u32>,
+    remote_id: Option<String>,
+    ssrc: Option<u64>,
+}
+
+impl From<&SysRtcStats> for InboundRtcStats {
+    fn from(val: &SysRtcStats) -> Self {
+        JsValue::from(val).into_serde().unwrap()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OutboundRtcStats {
+    bitrate_mean: Option<f32>,
+    bitrate_std_dev: Option<f32>,
+    bytes_sent: Option<u64>,
+    dropped_frames: Option<u32>,
+    fir_count: Option<u32>,
+    framerate_mean: Option<f32>,
+    framedrate_std_dev: Option<f32>,
+    framed_encoded: Option<u32>,
+    kind: Option<String>,
+    media_type: Option<String>,
+    nack_count: Option<u32>,
+    packets_sent: Option<u32>,
+    pli_count: Option<u32>,
+    qp_sum: Option<u32>,
+    remote_id: Option<String>,
+    ssrc: Option<u64>,
+}
+
+impl From<&SysRtcStats> for OutboundRtcStats {
+    fn from(val: &SysRtcStats) -> Self {
+        JsValue::from(val).into_serde().unwrap()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LocalCandidateStat {
+    network_type: Option<String>,
+    address: Option<String>,
+    transport_id: Option<String>,
+    port: Option<u16>,
+    protocol: Option<String>,
+    candidate_type: Option<String>,
+    priority: Option<u32>,
+    url: Option<String>,
+    relay_protocol: Option<String>,
+    deleted: Option<bool>,
+}
+
+impl From<&SysRtcStats> for LocalCandidateStat {
+    fn from(val: &SysRtcStats) -> Self {
+        JsValue::from(val).into_serde().unwrap()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RemoteCandidateStat {
+    transport_id: Option<String>,
+    network_type: Option<String>,
+    address: Option<String>,
+    port: Option<u16>,
+    protocol: Option<String>,
+    candidate_type: Option<String>,
+    priority: Option<u32>,
+    url: Option<String>,
+    relay_protocol: Option<String>,
+    deleted: Option<bool>,
+}
+
+impl From<&SysRtcStats> for RemoteCandidateStat {
+    fn from(val: &SysRtcStats) -> Self {
+        JsValue::from(val).into_serde().unwrap()
+    }
 }
