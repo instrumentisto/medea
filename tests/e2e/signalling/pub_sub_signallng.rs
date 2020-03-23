e1d2bb23c6f6b74b47311ec449d20c43947d10b7
Semen Evdokimov <evdokimovs@protonmail.ch>
2020-03-23 12:41:39 +0300

    Fix [run ci]

--- tests/e2e/signalling/pub_sub_signallng.rs
+++ tests/e2e/signalling/pub_sub_signallng.rs
@@ -3,17 +3,20 @@ use medea_client_api_proto::{Direction, Event};
 
 use crate::signalling::TestMember;
 
-#[actix_rt::test]
-async fn pub_sub_video_call() {
-    let base_url = "ws://127.0.0.1:8080/ws/pub-sub-video-call";
+#[test]
+fn pub_sub_video_call() {
+    System::run(|| {
+        let base_url = "ws://127.0.0.1:8080/ws/pub-sub-video-call";
 
-    // Note that events is separated by members.
-    // Every member will have different instance of this.
-    let test_fn = move |event: &Event,
-                        _: &mut Context<TestMember>,
-                        events: Vec<&Event>| {
-        // Start checking result of test.
-        if let Event::IceCandidateDiscovered { .. } = event {
+        // Note that events is separated by members.
+        // Every member will have different instance of this.
+        let test_fn = move |event: &Event,
+                            _: &mut Context<TestMember>,
+                            events: Vec<&Event>| {
+            println!(
+                "\n\n\n\n\n\n\n\n\n\nEvent: {:?}\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                event
+            );
             let peers_count = events
                 .iter()
                 .filter(|e| match e {
@@ -21,84 +24,92 @@ async fn pub_sub_video_call() {
                     _ => false,
                 })
                 .count();
+            if peers_count < 1 {
+                return;
+            }
             assert_eq!(peers_count, 1);
 
-            let is_caller;
-            if let Event::PeerCreated {
-                peer_id,
-                sdp_offer,
-                tracks,
-                ice_servers,
-                force_relay,
-            } = &events[0]
-            {
-                assert_eq!(ice_servers.len(), 2);
-                assert_eq!(
-                    ice_servers[0].urls[0],
-                    "stun:localhost:3478".to_string()
-                );
-                assert_eq!(
-                    ice_servers[1].urls[0],
-                    "turn:localhost:3478".to_string()
-                );
-                assert_eq!(
-                    ice_servers[1].urls[1],
-                    "turn:localhost:3478?transport=tcp".to_string()
-                );
-                assert_eq!(force_relay, &true);
+            // Start checking result of test.
+            if let Event::IceCandidateDiscovered { .. } = event {
+                let is_caller;
+                if let Event::PeerCreated {
+                    peer_id,
+                    sdp_offer,
+                    tracks,
+                    ice_servers,
+                    force_relay,
+                } = &events[0]
+                {
+                    assert_eq!(ice_servers.len(), 2);
+                    assert_eq!(
+                        ice_servers[0].urls[0],
+                        "stun:localhost:3478".to_string()
+                    );
+                    assert_eq!(
+                        ice_servers[1].urls[0],
+                        "turn:localhost:3478".to_string()
+                    );
+                    assert_eq!(
+                        ice_servers[1].urls[1],
+                        "turn:localhost:3478?transport=tcp".to_string()
+                    );
+                    assert_eq!(force_relay, &true);
 
-                if sdp_offer.is_some() {
-                    is_caller = false;
-                } else {
-                    is_caller = true;
-                }
-                assert_eq!(tracks.len(), 2);
-                for track in tracks {
-                    match &track.direction {
-                        Direction::Send { receivers, .. } => {
-                            assert!(is_caller);
-                            assert!(!receivers.contains(&peer_id));
-                        }
-                        Direction::Recv { sender, .. } => {
-                            assert!(!is_caller);
-                            assert_ne!(sender, peer_id);
+                    if sdp_offer.is_some() {
+                        is_caller = false;
+                    } else {
+                        is_caller = true;
+                    }
+                    assert_eq!(tracks.len(), 2);
+                    for track in tracks {
+                        match &track.direction {
+                            Direction::Send { receivers, .. } => {
+                                assert!(is_caller);
+                                assert!(!receivers.contains(&peer_id));
+                            }
+                            Direction::Recv { sender, .. } => {
+                                assert!(!is_caller);
+                                assert_ne!(sender, peer_id);
+                            }
                         }
                     }
+                } else {
+                    unreachable!()
                 }
-            } else {
-                unreachable!()
-            }
 
-            if is_caller {
-                if let Event::SdpAnswerMade { .. } = &events[1] {
+                if is_caller {
+                    if let Event::SdpAnswerMade { .. } = &events[1] {
+                    } else {
+                        unreachable!();
+                    }
+
+                    if let Event::IceCandidateDiscovered { .. } = &events[2] {
+                    } else {
+                        unreachable!();
+                    }
+                } else if let Event::IceCandidateDiscovered { .. } = &events[1]
+                {
                 } else {
                     unreachable!();
                 }
 
-                if let Event::IceCandidateDiscovered { .. } = &events[2] {
-                } else {
-                    unreachable!();
+                if is_caller {
+                    System::current().stop();
                 }
-            } else if let Event::IceCandidateDiscovered { .. } = &events[1] {
-            } else {
-                unreachable!();
             }
+        };
 
-            if is_caller {
-                System::current().stop();
-            }
-        }
-    };
-
-    let deadline = Some(std::time::Duration::from_secs(5));
-    TestMember::start(
-        format!("{}/caller/test", base_url),
-        Box::new(test_fn),
-        deadline,
-    );
-    TestMember::start(
-        format!("{}/responder/test", base_url),
-        Box::new(test_fn),
-        deadline,
-    );
+        let deadline = Some(std::time::Duration::from_secs(5));
+        TestMember::start(
+            format!("{}/caller/test", base_url),
+            Box::new(test_fn),
+            deadline,
+        );
+        TestMember::start(
+            format!("{}/responder/test", base_url),
+            Box::new(test_fn),
+            deadline,
+        );
+    })
+    .unwrap();
 }
