5e8b40a4b49a0ffbd6010ddd897002bbea139b7e
Semen Evdokimov <evdokimovs@protonmail.ch>
2020-04-01 18:07:05 +0300

    Add DTOs with accessor traits

--- proto/client-api/src/presenters/peer_presenter.rs
+++ proto/client-api/src/presenters/peer_presenter.rs
@@ -13,6 +13,67 @@ use crate::{
 };
 
 use super::TrackPresenter;
+use crate::presenters::track_presenter::{TrackSnapshot, TrackSnapshotAccessor};
+
+pub struct PeerSnapshot {
+    pub id: PeerId,
+    pub sdp_offer: Option<String>,
+    pub sdp_answer: Option<String>,
+    pub tracks: HashMap<TrackId, TrackSnapshot>,
+    pub ice_servers: Vec<IceServer>,
+    pub is_force_relayed: bool,
+    pub ice_candidates: Vec<IceCandidate>,
+}
+
+pub trait PeerSnapshotAccessor {
+    type Track: TrackSnapshotAccessor;
+
+    fn new(id: PeerId, sdp_offer: Option<String>, ice_servers: Vec<IceServer>, is_force_relayed: bool, tracks: HashMap<TrackId, Self::Track>) -> Self;
+
+    fn set_sdp_answer(&mut self, sdp_answer: String);
+
+    fn add_ice_candidate(&mut self, ice_candidate: IceCandidate);
+
+    fn update_track<F>(&mut self, track_id: TrackId, update_fn: F) where F: FnOnce(Option<&mut Self::Track>);
+
+    fn update_tracks(&mut self, patches: Vec<TrackPatch>) {
+        for patch in patches {
+            self.update_track(patch.id, |track| {
+                if let Some(track) = track {
+                    track.update(patch);
+                }
+            });
+        }
+    }
+}
+
+impl PeerSnapshotAccessor for PeerSnapshot {
+    type Track = TrackSnapshot;
+
+    fn new(id: PeerId, sdp_offer: Option<String>, ice_servers: Vec<IceServer>, is_force_relayed: bool, tracks: HashMap<TrackId, Self::Track>) -> Self {
+        Self {
+            id,
+            sdp_offer,
+            ice_servers,
+            is_force_relayed,
+            tracks,
+            sdp_answer: None,
+            ice_candidates: vec![],
+        }
+    }
+
+    fn set_sdp_answer(&mut self, sdp_answer: String) {
+        self.sdp_answer = Some(sdp_answer);
+    }
+
+    fn add_ice_candidate(&mut self, ice_candidate: IceCandidate) {
+        self.ice_candidates.push(ice_candidate);
+    }
+
+    fn update_track<F>(&mut self, track_id: TrackId, update_fn: F) where F: FnOnce(Option<&mut Self::Track>) {
+        (update_fn)(self.tracks.get_mut(&track_id));
+    }
+}
 
 #[derive(Debug)]
 pub struct PeerPresenter {
--- proto/client-api/src/presenters/track_presenter.rs
+++ proto/client-api/src/presenters/track_presenter.rs
@@ -12,6 +12,68 @@ use crate::{
     TrackId, TrackPatch,
 };
 
+pub struct TrackSnapshot {
+    pub id: TrackId,
+    pub is_muted: bool,
+    pub direction: Direction,
+    pub media_type: MediaType,
+}
+
+pub trait TrackSnapshotAccessor {
+    fn new(id: TrackId, is_muted: bool, direction: Direction, media_type: MediaType) -> Self;
+
+    fn update(&mut self, patch: TrackPatch) {
+        if let Some(is_muted) = patch.is_muted {
+            self.set_is_muted(is_muted);
+        }
+    }
+
+    fn set_is_muted(&mut self, is_muted: bool);
+
+    fn get_direction(&self) -> &Direction;
+
+    fn get_media_type(&self) -> &MediaType;
+
+    fn get_is_muted(&self) -> bool;
+
+    fn get_id(&self) -> TrackId;
+}
+
+impl TrackSnapshotAccessor for TrackSnapshot {
+    fn new(id: TrackId, is_muted: bool, direction: Direction, media_type: MediaType) -> Self {
+        Self {
+            id,
+            is_muted,
+            direction,
+            media_type,
+        }
+    }
+
+    fn set_is_muted(&mut self, is_muted: bool) {
+        self.is_muted = is_muted;
+    }
+
+    fn get_direction(&self) -> &Direction {
+        &self.direction
+    }
+
+    fn get_media_type(&self) -> &MediaType {
+        &self.media_type
+    }
+
+    fn get_is_muted(&self) -> bool {
+        self.is_muted
+    }
+
+    fn get_id(&self) -> TrackId {
+        self.id
+    }
+}
+
+
+
+
+
 #[derive(Debug)]
 pub struct TrackPresenter {
     pub(super) id: TrackId,
