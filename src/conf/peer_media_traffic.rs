e16cee1137fd4fd9e46d0534bee699781e759561
Semen Evdokimov <evdokimovs@protonmail.ch>
2020-04-16 15:26:17 +0300

    Config test and docs [run ci]

--- src/conf/peer_media_traffic.rs
+++ src/conf/peer_media_traffic.rs
@@ -29,3 +29,50 @@ pub struct PeerMediaTraffic {
     #[serde(with = "humantime_serde")]
     pub peer_init_timeout: Duration,
 }
+
+#[cfg(test)]
+mod spec {
+    use std::time::Duration;
+
+    use serial_test_derive::serial;
+
+    use crate::{conf::Conf, overrided_by_env_conf};
+
+    #[test]
+    #[serial]
+    fn overrides_defaults() {
+        let default_conf = Conf::default();
+        let env_conf = overrided_by_env_conf!(
+            "MEDEA_PEER_MEDIA_TRAFFIC__PEER_VALIDITY_TIMEOUT" => "501ms",
+            "MEDEA_PEER_MEDIA_TRAFFIC__TRAFFIC_FLOWING_TIMEOUT" => "502ms",
+            "MEDEA_PEER_MEDIA_TRAFFIC__PEER_INIT_TIMEOUT" => "503ms",
+        );
+
+        assert_ne!(
+            default_conf.peer_media_traffic.peer_validity_timeout,
+            env_conf.peer_media_traffic.peer_validity_timeout,
+        );
+        assert_eq!(
+            env_conf.peer_media_traffic.peer_validity_timeout,
+            Duration::from_millis(501)
+        );
+
+        assert_ne!(
+            default_conf.peer_media_traffic.traffic_flowing_timeout,
+            env_conf.peer_media_traffic.traffic_flowing_timeout,
+        );
+        assert_eq!(
+            env_conf.peer_media_traffic.traffic_flowing_timeout,
+            Duration::from_millis(502)
+        );
+
+        assert_ne!(
+            default_conf.peer_media_traffic.peer_init_timeout,
+            env_conf.peer_media_traffic.peer_init_timeout,
+        );
+        assert_eq!(
+            env_conf.peer_media_traffic.peer_init_timeout,
+            Duration::from_millis(503)
+        );
+    }
+}
