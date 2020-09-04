8365d18176151da11bb542bb81e615abe9fc5f3c
Semen Evdokimov <evdokimovs@protonmail.ch>
2020-09-04 19:38:03 +0300

    Refactor

--- src/signalling/peers/metrics/ice_restart_detector.rs
+++ src/signalling/peers/metrics/ice_restart_detector.rs
@@ -7,54 +7,68 @@ use crate::media::PeerStateMachine;
 
 use super::{PeersMetricsEvent, RtcStatsHandler};
 use crate::signalling::peers::metrics::EventSender;
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
 
-#[derive(Debug)]
-struct Peer {
-    id: PeerId,
-    partner_peer: Weak<RefCell<Peer>>,
-    connection_state: PeerConnectionState,
-}
+use self::peer_state::PeerState;
 
-impl Peer {
-    pub fn new_pair(
-        first_peer_id: PeerId,
-        second_peer_id: PeerId,
-    ) -> (Rc<RefCell<Peer>>, Rc<RefCell<Peer>>) {
-        let first_peer = Rc::new(RefCell::new(Peer {
-            id: first_peer_id,
-            partner_peer: Weak::default(),
-            connection_state: PeerConnectionState::New,
-        }));
-        let second_peer = Rc::new(RefCell::new(Peer {
-            id: second_peer_id,
-            partner_peer: Rc::downgrade(&first_peer),
-            connection_state: PeerConnectionState::New,
-        }));
-        first_peer.borrow_mut().partner_peer = Rc::downgrade(&second_peer);
+mod peer_state {
+    use std::{
+        cell::RefCell,
+        rc::{Rc, Weak},
+    };
 
-        (first_peer, second_peer)
+    use medea_client_api_proto::{PeerConnectionState, PeerId};
+
+    #[derive(Debug)]
+    struct Inner {
+        id: PeerId,
+        partner_peer: Weak<RefCell<Inner>>,
+        connection_state: PeerConnectionState,
     }
 
-    pub fn partner_peer(&self) -> Rc<RefCell<Self>> {
-        self.partner_peer.upgrade().unwrap()
-    }
+    #[derive(Debug)]
+    pub struct PeerState(Rc<RefCell<Inner>>);
 
-    pub fn update_connection_state(&mut self, new_state: PeerConnectionState) {
-        self.connection_state = new_state;
-    }
+    impl PeerState {
+        pub fn new_pair(
+            first_peer_id: PeerId,
+            second_peer_id: PeerId,
+        ) -> (Self, Self) {
+            let first_peer = Rc::new(RefCell::new(Inner {
+                id: first_peer_id,
+                partner_peer: Weak::default(),
+                connection_state: PeerConnectionState::New,
+            }));
+            let second_peer = Rc::new(RefCell::new(Inner {
+                id: second_peer_id,
+                partner_peer: Rc::downgrade(&first_peer),
+                connection_state: PeerConnectionState::New,
+            }));
+            first_peer.borrow_mut().partner_peer = Rc::downgrade(&second_peer);
 
-    pub fn id(&self) -> PeerId {
-        self.id
+            (Self(first_peer), Self(second_peer))
+        }
+
+        pub fn partner_peer(&self) -> Self {
+            Self(self.0.borrow().partner_peer.upgrade().unwrap())
+        }
+
+        pub fn connection_state(&self) -> PeerConnectionState {
+            self.0.borrow().connection_state
+        }
+
+        pub fn update_connection_state(&self, new_state: PeerConnectionState) {
+            self.0.borrow_mut().connection_state = new_state;
+        }
+
+        pub fn id(&self) -> PeerId {
+            self.0.borrow().id
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct IceRestartDetector {
-    peers: HashMap<PeerId, Rc<RefCell<Peer>>>,
+    peers: HashMap<PeerId, PeerState>,
     event_sender: EventSender,
 }
 
@@ -63,7 +77,8 @@ impl RtcStatsHandler for IceRestartDetector {
         let peer_id = peer.id();
         if !self.peers.contains_key(&peer_id) {
             let partner_peer_id = peer.partner_peer_id();
-            let (peer, partner_peer) = Peer::new_pair(peer_id, partner_peer_id);
+            let (peer, partner_peer) =
+                PeerState::new_pair(peer_id, partner_peer_id);
 
             self.peers.insert(peer_id, peer);
             self.peers.insert(partner_peer_id, partner_peer);
@@ -73,8 +88,7 @@ impl RtcStatsHandler for IceRestartDetector {
     fn unregister_peers(&mut self, peers_ids: &[PeerId]) {
         for peer_id in peers_ids {
             if let Some(peer) = self.peers.remove(peer_id) {
-                self.peers
-                    .remove(&peer.borrow().partner_peer().borrow().id());
+                self.peers.remove(&peer.partner_peer().id());
             }
         }
     }
@@ -86,15 +100,12 @@ impl RtcStatsHandler for IceRestartDetector {
     ) {
         if let Some(peer) = self.peers.get(&peer_id) {
             if let PeerConnectionState::Failed = new_state {
-                let old_state = peer.borrow().connection_state;
+                let old_state = peer.connection_state();
                 match old_state {
                     PeerConnectionState::Connected
                     | PeerConnectionState::Disconnected => {
-                        let partner_state = peer
-                            .borrow()
-                            .partner_peer()
-                            .borrow()
-                            .connection_state;
+                        let partner_state =
+                            peer.partner_peer().connection_state();
                         if let PeerConnectionState::Failed = partner_state {
                             self.event_sender.send_event(
                                 PeersMetricsEvent::IceRestartNeeded { peer_id },
@@ -104,7 +115,7 @@ impl RtcStatsHandler for IceRestartDetector {
                     _ => (),
                 }
             }
-            peer.borrow_mut().update_connection_state(new_state);
+            peer.update_connection_state(new_state);
         }
     }
 
