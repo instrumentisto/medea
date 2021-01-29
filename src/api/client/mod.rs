8cb668f88a5b6e7178b1dddc1a6a7ea723b9ca5f
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-01-29 19:14:33 +0300

    Decrease max WS message size to 512Kb

--- src/api/client/mod.rs
+++ src/api/client/mod.rs
@@ -14,5 +14,5 @@ pub use self::session::RpcServerRepository;
 /// This limit also is used for a fragmented message.
 ///
 /// `Room` state of 5 `Member`s with a screen sharing, camera and audio will be
-/// around 300 Kb, so this value is multiplied by 3 and rounded just in case.
-const MAX_WS_MSG_SIZE: usize = 1024 * 1024; // 1 Mb
+/// around 100 Kb, so this value is multiplied by 5 and rounded just in case.
+const MAX_WS_MSG_SIZE: usize = 512 * 1024; // 512 Kb
