dcc11ff8b7b200dd3c590e38bea5804f3def40a9
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-03-08 09:29:50 -0500

    Test

--- jason/src/peer/mod.rs
+++ jason/src/peer/mod.rs
@@ -394,15 +394,11 @@ impl PeerConnection {
         let media_connections = Rc::clone(&peer.media_connections);
         peer.peer
             .on_track(Some(move |track_event| {
-                let media_connections = Rc::clone(&media_connections);
-                spawn_local(async move {
-                    while let Err(err) =
-                        media_connections.add_remote_track(&track_event)
-                    {
-                        delay_for(Duration::from_millis(50).into()).await;
-                        JasonError::from(err).print();
-                    }
-                })
+                if let Err(err) =
+                    media_connections.add_remote_track(&track_event)
+                {
+                    JasonError::from(err).print();
+                };
             }))
             .map_err(tracerr::map_from_and_wrap!())?;
 
