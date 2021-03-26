9d88583262588f16fd7cc39c1e7a4a02f1b799cf
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-03-26 09:50:55 -0500

    Add some Flutter specific code

--- jason/jason-hello-world/src/lib.rs
+++ jason/jason-hello-world/src/lib.rs
@@ -1,3 +1,16 @@
+#[link(name = "trampoline")]
+extern "C" {
+    fn Dart_InitializeApiDL(obj: *mut libc::c_void) -> libc::intptr_t;
+    fn Dart_NewPersistentHandle_DL_Trampolined(object: Dart_Handle) -> Dart_PersistentHandle;
+    fn Dart_HandleFromPersistent_DL_Trampolined(object: Dart_PersistentHandle) -> Dart_Handle;
+    fn Dart_DeletePersistentHandle_DL_Trampolined(object: Dart_PersistentHandle);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn InitDartApiDL(obj: *mut libc::c_void) -> libc::intptr_t {
+    return Dart_InitializeApiDL(obj);
+}
+
 #[no_mangle]
 pub extern "C" fn add(i: i64) -> i64 {
     i + 100
