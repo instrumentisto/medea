eb08f1c181e4163ac2b6f6747ffdc89702701cca
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-04-15 12:54:41 -0500

    Add Callbacks functionality

--- jason/src/platform/dart/utils/callback.rs
+++ jason/src/platform/dart/utils/callback.rs
@@ -1,6 +1,7 @@
 use std::{cell::RefCell, marker::PhantomData};
 
 use dart_sys::Dart_Handle;
+use std::any::Any;
 
 pub struct Callback<A>(RefCell<Option<Function<A>>>);
 
@@ -38,8 +39,55 @@ impl Function<()> {
     }
 }
 
-impl<A> Function<A> {
-    pub fn call1(&self, arg: A) {
-        todo!()
+type CallRustObjectFunction = extern "C" fn(Dart_Handle, *mut dyn Any);
+static mut CALL_RUST_OBJECT_FUNCTION: Option<CallRustObjectFunction> = None;
+
+#[no_mangle]
+pub unsafe extern "C" fn register_call_rust_object_function(
+    f: CallRustObjectFunction,
+) {
+    CALL_RUST_OBJECT_FUNCTION = Some(f);
+}
+
+impl<A: 'static> Function<Box<A>> {
+    pub fn call1(&self, arg: Box<A>) {
+        let arg = Box::into_raw(arg);
+        unsafe { CALL_RUST_OBJECT_FUNCTION.unwrap()(self.handle, arg) }
+    }
+}
+
+type CallDartHandleFunction = extern "C" fn(Dart_Handle, Dart_Handle);
+static mut CALL_DART_HANDLE_FUNCTION: Option<CallDartHandleFunction> = None;
+
+#[no_mangle]
+pub unsafe extern "C" fn register_call_dart_handle_function(
+    f: CallDartHandleFunction,
+) {
+    CALL_DART_HANDLE_FUNCTION = Some(f)
+}
+
+impl Function<Dart_Handle> {
+    pub fn call_with_dart_handle(&self, arg: Dart_Handle) {
+        unsafe {
+            CALL_DART_HANDLE_FUNCTION.unwrap()(self.handle, arg);
+        }
+    }
+}
+
+type CallIntFunction = extern "C" fn(Dart_Handle, i32);
+static mut CALL_INT_FUNCTION: Option<CallIntFunction> = None;
+
+#[no_mangle]
+pub unsafe extern "C" fn register_call_int_function(
+    f: CallIntFunction,
+) {
+    CALL_INT_FUNCTION = Some(f);
+}
+
+impl Function<i32> {
+    pub fn call_with_int(&self, int: i32) {
+        unsafe {
+            CALL_INT_FUNCTION.unwrap()(self.handle, int);
+        }
     }
 }
