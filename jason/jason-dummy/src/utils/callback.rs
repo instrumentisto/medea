8fb929723d647e1b9d08af64bc2aff4fd50e25b2
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-04-20 09:19:45 -0500

    Add some types for DartCallback

--- jason/jason-dummy/src/utils/callback.rs
+++ jason/jason-dummy/src/utils/callback.rs
@@ -8,13 +8,13 @@ use super::trampoline::{
     Dart_NewPersistentHandle_DL_Trampolined,
 };
 
-pub type AnyClosureCaller = extern "C" fn(c: Dart_Handle, var: *mut dyn Any);
-
-static mut ANY_CLOSURE_CALLER: Option<AnyClosureCaller> = None;
-
-pub unsafe fn set_any_closure_caller(caller: AnyClosureCaller) {
-    ANY_CLOSURE_CALLER = Some(caller);
+pub type PointerClosureCaller = extern "C" fn(c: Dart_Handle, var: *mut dyn Any);
+static mut POINTER_CLOSURE_CALLER: Option<PointerClosureCaller> = None;
+#[no_mangle]
+pub unsafe extern "C" fn register_pointer_closure_caller(caller: PointerClosureCaller) {
+    POINTER_CLOSURE_CALLER = Some(caller);
 }
+
 
 pub struct DartCallback<T> {
     cb: Dart_PersistentHandle,
@@ -31,12 +31,46 @@ impl<T: 'static> DartCallback<T> {
         }
     }
 
-    pub unsafe fn call(&self, arg: T) {
-        let closure_handle = Dart_HandleFromPersistent_DL_Trampolined(self.cb);
-        ANY_CLOSURE_CALLER.unwrap()(
-            closure_handle,
-            Box::into_raw(Box::new(arg) as Box<dyn Any>),
-        );
+    pub fn call(&self, arg: T) {
+        unsafe {
+            let closure_handle = Dart_HandleFromPersistent_DL_Trampolined(self.cb);
+            POINTER_CLOSURE_CALLER.unwrap()(
+                closure_handle,
+                Box::into_raw(Box::new(arg) as Box<dyn Any>),
+            );
+        }
+    }
+}
+
+type UnitClosureCaller = extern "C" fn(Dart_Handle);
+static mut UNIT_CLOSURE_CALLER: Option<UnitClosureCaller> = None;
+#[no_mangle]
+pub unsafe extern "C" fn register_unit_closure_caller(f: UnitClosureCaller) {
+    UNIT_CLOSURE_CALLER = Some(f)
+}
+
+impl DartCallback<()> {
+    pub fn call_unit(&self) {
+        unsafe {
+            let closure_handle = Dart_HandleFromPersistent_DL_Trampolined(self.cb);
+            UNIT_CLOSURE_CALLER .unwrap()(closure_handle);
+        }
+    }
+}
+
+type IntClosureCaller = extern "C" fn(Dart_Handle, i32);
+static mut INT_CLOSURE_CALLER: Option<IntClosureCaller> = None;
+#[no_mangle]
+pub unsafe extern "C" fn register_int_closure_caller(f: IntClosureCaller) {
+    INT_CLOSURE_CALLER = Some(f);
+}
+
+impl DartCallback<i32> {
+    pub fn call_int(&self, arg: i32) {
+        unsafe {
+            let closure_handle = Dart_HandleFromPersistent_DL_Trampolined(self.cb);
+            INT_CLOSURE_CALLER .unwrap()(closure_handle, arg);
+        }
     }
 }
 
