b3932a97aeef2654bac0eb586d89210eeb2373ae
tyranron <tyranron@gmail.com>
2021-05-17 17:13:04 +0300

    Corrections

--- jason/src/media/track/mod.rs
+++ jason/src/media/track/mod.rs
@@ -28,10 +28,10 @@ pub enum MediaStreamTrackState {
 #[repr(u8)]
 pub enum MediaSourceKind {
     /// Media is sourced from some media device (webcam or microphone).
-    Device,
+    Device = 0,
 
     /// Media is obtained with screen-capture.
-    Display,
+    Display = 1,
 }
 
 impl From<MediaSourceKind> for proto::MediaSourceKind {
